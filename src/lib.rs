c0cbb3862600155ad2e5608a14e4f685ad953ded
Adrian Vovk <adrianvovk@gmail.com>
2023-11-01 13:22:31 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,11 @@ pub trait LockLevel {
 pub trait LockLevelBelow<HigherLock: LockLevel> {}
 
 pub trait Locks<Lock: LockLevel> {
-    fn lock(self, other: &Lock) -> LockResult<(Handle<Lock>, MutexGuard<Lock::Data>)>;
-    fn try_lock(self, other: &Lock) -> TryLockResult<(Handle<Lock>, MutexGuard<Lock::Data>)>;
-    fn call<T>(&self, ret: (Token<Lock>, T)) -> T;
-
-    fn with<T, F>(self, other: &Lock, cb: F) -> T
+    fn locks<T, F>(self, other: &Lock, cb: F) -> T
     where
         F: FnOnce(Handle<Lock>, &mut Lock::Data) -> T;
+
+    fn call<T>(&self, ret: (Token<Lock>, T)) -> T;
 }
 
 pub struct Handle<T>(PhantomData<T>);
@@ -46,71 +44,21 @@ where
     HigherLock: LockLevel,
     LowerLock: LockLevel + LockLevelBelow<HigherLock>,
 {
-    fn lock(
-        self,
-        child: &LowerLock,
-    ) -> LockResult<(Handle<LowerLock>, MutexGuard<LowerLock::Data>)> {
-        let child_handle = unsafe { Handle::new(child) };
-        let child_guard = unsafe { child.lock() };
-        match child_guard {
-            Ok(res) => Ok((child_handle, res)),
-            Err(e) => Err(PoisonError::new((child_handle, e.into_inner()))),
-        }
-    }
-
-    fn try_lock(
-        self,
-        child: &LowerLock,
-    ) -> TryLockResult<(Handle<LowerLock>, MutexGuard<LowerLock::Data>)> {
-        let child_handle = unsafe { Handle::new(child) };
-        let child_guard = unsafe { child.try_lock() };
-        match child_guard {
-            Ok(res) => Ok((child_handle, res)),
-            Err(TryLockError::Poisoned(e)) => {
-                Err(PoisonError::new((child_handle, e.into_inner())).into())
-            }
-            Err(TryLockError::WouldBlock) => Err(TryLockError::WouldBlock),
-        }
+    fn locks<T, F>(self, child: &LowerLock, cb: F) -> T
+    where
+        F: FnOnce(Handle<LowerLock>, &mut LowerLock::Data) -> T,
+    {
+        let handle = unsafe {Handle::new(child)};
+        let mut guard = unsafe { child.lock() }.unwrap(); // TODO: Error handling
+        cb(handle, &mut guard)
     }
 
     fn call<T>(&self, ret: (Token<LowerLock>, T)) -> T {
         ret.1
     }
-
-    fn with<T, F>(self, other: &LowerLock, cb: F) -> T
-    where
-        F: FnOnce(Handle<LowerLock>, &mut LowerLock::Data) -> T,
-    {
-        let (hdl, mut guard) = self.lock(other).unwrap();
-        cb(hdl, &mut guard)
-    }
 }
 
 trait UseOnlyOnce {} // TODO?
-
-#[macro_export]
-macro_rules! baselock {
-    (let ($handle:ident, $data:ident) = lock($a:expr)) => {
-        let $handle = unsafe { Handle::new($a) };
-        let $data = unsafe { $a.lock() };
-        macro_rules! baselock {
-            () => {};
-        }
-        //impl<T: LockLevel> UseOnlyOnce for T {}
-        // TODO!!!! ^^^^^
-        // Idea 1: Just take variable name idents and declare the vars in the macro
-    };
-
-    // Convenience macros for dealing w/ the Result
-    (let ($handle:ident, $data:ident) = lock($a:expr)?) => {
-        baselock!(let ($handle, $data) = lock($a));
-        let $data = $data?;
-    };
-    (let ($handle:ident, $data:ident) = lock($a:expr).unwrap()) => {
-        baselock!(let ($handle, $data) = lock($a));
-        let $data = $data.unwrap();
-    };
-}
 
 #[macro_export]
 macro_rules! baselock2 {
