7802b66360df70a4e4c9cf1852a16c2a159074b6
rene <renedes@protonmail.com>
2023-11-03 15:01:59 -0400

    Basic implmenetation of thread centric

---
+++ src/bin/lennon.rs
@@ -0,0 +1,33 @@
+use locks::*;
+use std::sync::Arc;
+
+define_level!(S);
+define_level!(A);
+impl<T, U> LockLevelBelow<S<T>> for A<U> {}
+define_level!(B);
+impl<T, U> LockLevelBelow<A<T>> for B<U> {}
+impl<T, U> LockLevelBelow<S<T>> for B<U> {}
+
+fn main() {
+    let s = Arc::new(S::new(()));
+    let a = Arc::new(A::new(10));
+    let b = Arc::new(B::new(2));
+
+    let s_clone = Arc::clone(&s);
+    let a_clone = Arc::clone(&a);
+    let b_clone = Arc::clone(&b);
+
+    spawn(&*s, move |mut s_hdl| loop {
+        s_hdl.locks(&*a, |mut a_hdl, a_data| {
+            a_hdl.locks(&*b, |_, b_data| println!("{a_data}, {b_data}"));
+        });
+    });
+
+    spawn(&*s_clone, move |mut s_hdl| loop {
+        s_hdl.locks(&*a_clone, |mut a_hdl, a_data| {
+            a_hdl.locks(&*b_clone, |_, b_data| println!("{a_data}, {b_data}"));
+        });
+    })
+    .join()
+    .unwrap()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{
     marker::PhantomData,
     sync::{PoisonError, TryLockError},
+    thread::{self, JoinHandle},
 };
 
 pub use std::sync::{LockResult, Mutex, MutexGuard, TryLockResult};
@@ -15,11 +16,9 @@ pub trait LockLevel {
 pub trait LockLevelBelow<HigherLock: LockLevel> {}
 
 pub trait Locks<Lock: LockLevel> {
-    fn locks<T, F>(self, other: &Lock, cb: F) -> T
+    fn locks<T, F>(&mut self, other: &Lock, cb: F) -> T
     where
         F: FnOnce(Handle<Lock>, &mut Lock::Data) -> T;
-
-    fn call<T>(&self, ret: (Token<Lock>, T)) -> T;
 }
 
 pub struct Handle<T>(PhantomData<T>);
@@ -44,42 +43,25 @@ where
     HigherLock: LockLevel,
     LowerLock: LockLevel + LockLevelBelow<HigherLock>,
 {
-    fn locks<T, F>(self, child: &LowerLock, cb: F) -> T
+    fn locks<T, F>(&mut self, child: &LowerLock, cb: F) -> T
     where
         F: FnOnce(Handle<LowerLock>, &mut LowerLock::Data) -> T,
     {
-        let handle = unsafe {Handle::new(child)};
+        let handle = unsafe { Handle::new(child) };
         let mut guard = unsafe { child.lock() }.unwrap(); // TODO: Error handling
         cb(handle, &mut guard)
     }
-
-    fn call<T>(&self, ret: (Token<LowerLock>, T)) -> T {
-        ret.1
-    }
 }
 
-trait UseOnlyOnce {} // TODO?
+pub fn spawn<F, T, BaseLock>(base: &BaseLock, f: F) -> JoinHandle<T>
+where
+    BaseLock: LockLevel + Send + 'static,
+    F: FnOnce(Handle<BaseLock>) -> T + Send + 'static,
+    T: Send + 'static,
+{
+    let handle = unsafe { Handle::new(base) };
 
-#[macro_export]
-macro_rules! baselock2 {
-    (let ($token:ident, $handle:ident, $data:ident) = lock($a:expr)) => {
-        let $token = unsafe { Token::new($a) };
-        let $handle = unsafe { Handle::new($a) };
-        let $data = unsafe { $a.lock() };
-        macro_rules! baselock2 {
-            () => {};
-        }
-    };
-
-    // Convenience macros for dealing w/ the Result
-    (let ($token:ident, $handle:ident, $data:ident) = lock($a:expr)?) => {
-        baselock2!(let ($token, $handle, $data) = lock($a));
-        let $data = $data?;
-    };
-    (let ($token:ident, $handle:ident, $data:ident) = lock($a:expr).unwrap()) => {
-        baselock2!(let ($token, $handle, $data) = lock($a));
-        let $data = $data.unwrap();
-    };
+    thread::spawn(|| f(handle))
 }
 
 // TODO: temporary
